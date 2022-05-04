ce9622f743cf945da85f6b69da43cbb5b2c48cd3
monoid <qaswedfr55@gmail.com>
2022-05-05 00:46:30 +0900

    feat: add chunk.move

--- backend/src/rpc/chunk.ts
+++ backend/src/rpc/chunk.ts
@@ -29,6 +29,7 @@ export async function handleChunkMethod(
           new ChunkConflictError(doc.chunks, doc.updatedAt),
         );
       }
+
       const chunk = {
         id: p.params.chunkId || makeChunkId(),
         ...p.params.chunkContent,
@@ -42,25 +43,40 @@ export async function handleChunkMethod(
         return makeRPCError(p.id, new InvalidChunkIdError(p.params.chunkId));
       }
       doc.chunks.splice(p.params.position, 0, chunk);
+
       doc.updateDocHistory(p);
       doc.broadcastMethod(p, doc.updatedAt, conn);
+
       return makeRPCResult(p.id, { chunkId: chunk.id });
     }
     case "chunk.delete": {
       const doc = await DocStore.open(conn, docPath);
+      
       const chunkIndex = doc.chunks.findIndex((c) => c.id === p.params.chunkId);
       if (chunkIndex < 0) {
         return makeRPCError(p.id, new InvalidChunkIdError(p.params.chunkId));
       }
       doc.chunks.splice(chunkIndex, 1);
+
       doc.updateDocHistory(p);
       doc.broadcastMethod(p, doc.updatedAt, conn);
+
       return makeRPCResult(p.id, {
         chunkId: p.params.chunkId,
       });
     }
     case "chunk.modify": {
       const doc = await DocStore.open(conn, docPath);
+      
+      //is params.doc stale?
+      if (doc.updatedAt > updateAt) {
+        // if so, reject with error
+        return makeRPCError(
+          p.id,
+          new ChunkConflictError(doc.chunks, doc.updatedAt),
+        );
+      }
+
       const chunkIndex = doc.chunks.findIndex((c) => c.id === p.params.chunkId);
       if (chunkIndex < 0) {
         return makeRPCError(p.id, new InvalidChunkIdError(p.params.chunkId));
@@ -69,6 +85,38 @@ export async function handleChunkMethod(
         ...doc.chunks[chunkIndex],
         ...p.params.chunkContent,
       };
+
+      doc.updateDocHistory(p);
+      doc.broadcastMethod(p, doc.updatedAt, conn);
+
+      return makeRPCResult(p.id, {
+        chunkId: p.params.chunkId,
+      });
+    }
+    case "chunk.move": {
+      const doc = await DocStore.open(conn, docPath);
+
+      //is params.doc stale?
+      if (doc.updatedAt > updateAt) {
+        // if so, reject with error
+        return makeRPCError(
+          p.id,
+          new ChunkConflictError(doc.chunks, doc.updatedAt),
+        );
+      }
+
+      const chunkIndex = doc.chunks.findIndex((c) => c.id === p.params.chunkId);
+      if (chunkIndex < 0) {
+        return makeRPCError(p.id, new InvalidChunkIdError(p.params.chunkId));
+      }
+      if (p.params.position > doc.chunks.length || p.params.position < 0) {
+        return makeRPCError(p.id, new InvalidPositionError(p.params.position));
+      }
+
+      const chunk = doc.chunks[chunkIndex];
+      doc.chunks.splice(chunkIndex, 1);
+      doc.chunks.splice(p.params.position, 0, chunk);
+
       doc.updateDocHistory(p);
       doc.broadcastMethod(p, doc.updatedAt, conn);
       return makeRPCResult(p.id, {
@@ -76,5 +124,4 @@ export async function handleChunkMethod(
       });
     }
   }
-  throw new Error("Not implemented");
 }
