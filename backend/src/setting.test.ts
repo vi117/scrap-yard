b8eda615532c2c7a6e2c90f9bd993ea3802fc2be
monoid <qaswedfr55@gmail.com>
2022-05-11 20:17:23 +0900

    setting rework

--- backend/src/setting.test.ts
+++ backend/src/setting.test.ts
@@ -1,33 +1,22 @@
-import { loadSetting, registerSetting } from "./setting.ts";
+import * as setting from "./setting.ts";
 import { assertEquals, assertRejects } from "std/assert";
-import { returnsNext, stub } from "std/mock";
 
 Deno.test({
   name: "setting: basic",
   fn: async () => {
-    const envStub = stub(
-      Deno.env,
-      "get",
-      returnsNext([
-        "src/testdata/test1_setting.json",
-        "src/testdata/test1_setting.json",
-      ]),
-    );
-    try {
-      registerSetting("test", {
-        type: "object",
-        properties: {
-          a: { type: "string" },
-          b: { type: "number" },
-        },
-      });
-      const data = await loadSetting<{ a: string; b: number }>("test");
-      assertEquals(data, { a: "a", b: 1 });
-      await assertRejects(async () => {
-        await loadSetting<{ a: string; b: number }>("test2");
-      }, Error);
-    } finally {
-      envStub.restore();
-    }
+    setting.setPath("src/testdata/test1_setting.json");
+    setting.register("test", {
+      type: "object",
+      properties: {
+        a: { type: "string" },
+        b: { type: "number" },
+      },
+    });
+    await setting.load();
+    const data = setting.get<{ a: string; b: number }>("test");
+    assertEquals(data, { a: "a", b: 1 });
+    await assertRejects(async () => {
+      await setting.get<{ a: string; b: number }>("test2");
+    }, Error);
   },
 });
