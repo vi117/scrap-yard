661c6faa2685606ab33277a46b2d37aa1d73f70f
Hyeonung Baek <wiserlix@gmail.com>
2022-05-14 19:06:03 +0900

    Change Autocomplete in Settings to Select

--- frontend/src/Component/Settings.tsx
+++ frontend/src/Component/Settings.tsx
@@ -1,22 +1,54 @@
-import { Autocomplete, Button, Dialog, DialogTitle, List, ListItem, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Dialog,
+  DialogTitle,
+  InputLabel,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import React, { useState } from "react";
+
+// TODO: move these consts to datas
+const languages = ["korean", "english"];
+const themes = ["light", "dark"];
+
+const context = {
+  language: "korean",
+  theme: "light",
+};
 
 export function Settings(props: {
   open: boolean;
   onClose: () => void;
 }) {
   const langSetting = (
-    <Autocomplete
-      options={["korean", "english"]}
-      renderInput={(p) => <TextField {...p} label="Language" />}
-    />
+    <>
+      <InputLabel id="settings-language-select-label">Language</InputLabel>
+      <Select
+        labelId="settings-language-select-label"
+        value={context.language}
+        onChange={() => {}} // TODO: fill here
+      >
+        {languages.map((l) => <MenuItem key={l} value={l}>{l}</MenuItem>)}
+      </Select>
+    </>
   );
 
   const themeSetting = (
-    <Autocomplete
-      options={["light", "dark"]}
-      renderInput={(p) => <TextField {...p} label="Theme" />}
-    />
+    <>
+      <InputLabel id="settings-theme-select-label">Theme</InputLabel>
+      <Select
+        labelId="settings-theme-select-label"
+        value={context.theme}
+        onChange={() => {}} // TODO: fill here
+      >
+        {themes.map((t) => <MenuItem key={t} value={t}>{t}</MenuItem>)}
+      </Select>
+    </>
   );
 
   return (
