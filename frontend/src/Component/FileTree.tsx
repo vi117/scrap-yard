f45cafe467659304b27b6703b350d83c0c2fbaa5
Hyeonung Baek <wiserlix@gmail.com>
2022-05-19 12:55:06 +0900

    Split FileTree & add custom TreeItem DirItem

--- frontend/src/Component/FileTree.tsx
+++ frontend/src/Component/FileTree.tsx
@@ -1,47 +1,89 @@
-import { TreeItem, TreeView } from "@mui/lab";
-import { Drawer } from "@mui/material";
-import { useEffect, useState } from "react";
+import ArticleIcon from "@mui/icons-material/Article";
+import FolderIcon from "@mui/icons-material/Folder";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import TreeItem, { TreeItemContentProps, TreeItemProps, useTreeItem } from "@mui/lab/TreeItem";
+import TreeView from "@mui/lab/TreeView";
+import { Button, Drawer, IconButton, Typography } from "@mui/material";
+import clsx from "clsx";
+import React, { forwardRef, useEffect, useState } from "react";
 
-import { FsManager } from "../Model/FsManager";
-import { RPCManager as rpcman } from "../Model/RPCManager";
+import { DirTree, useDirTree } from "./dirTree";
 
-const manager = new FsManager(rpcman);
+const DirContent = forwardRef(function DirContent(
+  props: TreeItemContentProps & { onClick: () => void },
+  ref,
+) {
+  const {
+    classes,
+    className,
+    label,
+    nodeId,
+    icon,
+    onClick,
+  } = props;
 
-async function collectDirTree(path: string, name: string) {
-  const res = await manager.getStat(path);
+  const {
+    disabled,
+    expanded,
+    selected,
+    focused,
+    handleExpansion,
+    handleSelection,
+    preventSelection,
+  } = useTreeItem(nodeId);
 
-  if (res.isDirectory) {
-    const proms = res.entries.map(async (entry) => {
-      return await collectDirTree(path + "/" + entry.name, entry.name);
-    });
-
-    const children = await Promise.all(proms);
-
-    return { name: name, children };
-  } else {
-    return { name: name };
-  }
-}
-
-function useDirTree(path) {
-  const [dirtree, setDirtree] = useState(null);
-
-  const getDirTree = async () => {
-    const d = await collectDirTree(path, "/");
-    setDirtree(d);
+  const handleMouseDown = (event) => {
+    preventSelection(event);
   };
 
-  useEffect(() => {
-    if (!dirtree) {
-      getDirTree();
-    }
-  });
+  const handleClick = (event) => {
+    handleExpansion(event);
+    handleSelection(event);
+    onClick();
+  };
 
-  return dirtree;
-}
+  return (
+    <div
+      className={clsx(className, classes.root, {
+        [classes.expanded]: expanded,
+        [classes.selected]: selected,
+        [classes.focused]: focused,
+        [classes.disabled]: disabled,
+      })}
+      ref={ref as React.Ref<HTMLDivElement>}
+      onClick={handleClick}
+      onMouseDown={handleMouseDown}
+      style={{
+        display: "flex",
+      }}
+    >
+      <div className={classes.iconContainer}>
+        {icon}
+      </div>
+
+      <Typography
+        component="div"
+        className={classes.label}
+      >
+        {label}
+      </Typography>
+
+      <IconButton
+        onClick={() => console.log("not implemented")}
+        sx={{ marginLeft: "auto" }}
+      >
+        <MoreVertIcon fontSize="small" />
+      </IconButton>
+    </div>
+  );
+});
+
+const DirItem = (props: TreeItemProps & { onClick: () => void }) => (
+  <TreeItem ContentComponent={DirContent} {...props} />
+);
 
 export function FileTreeInner(props: {
-  dirTree;
+  dirTree: DirTree;
   width: number;
   open: boolean;
   onClick: (f: string) => void;
@@ -50,18 +92,19 @@ export function FileTreeInner(props: {
 }) {
   const dirTree = props.dirTree;
 
-  const renderTree = (node) => {
+  const renderTree = (node: DirTree) => {
     return (
-      <TreeItem
+      <DirItem
         key={node.name}
         nodeId={node.name}
         label={node.name}
         onClick={() => props.onClick(node.name)}
+        icon={node.type == "dir" ? <FolderIcon /> : <ArticleIcon />}
       >
         {Array.isArray(node.children)
           ? node.children.map(renderTree)
           : null}
-      </TreeItem>
+      </DirItem>
     );
   };
 
