483053832789357e854025322ded309692822f09
Hyeonung Baek <wiserlix@gmail.com>
2022-05-24 18:43:56 +0900

    Add CORS Header

--- backend/src/router/methodHandle.ts
+++ backend/src/router/methodHandle.ts
@@ -5,25 +5,31 @@ type MethodHandler = Record<string, Handler>;
 
 export class MethodHandlerBuilber {
     handlers: MethodHandler;
+
     constructor() {
         this.handlers = {};
     }
+
     get(handler: Handler) {
         this.handlers.get = handler;
         return this;
     }
+
     post(handler: Handler) {
         this.handlers.post = handler;
         return this;
     }
+
     put(handler: Handler) {
         this.handlers.put = handler;
         return this;
     }
+
     delete(handler: Handler) {
         this.handlers.delete = handler;
         return this;
     }
+
     build(): Handler {
         return (req, ctx) => {
             const method = req.method.toLowerCase();
@@ -33,6 +39,15 @@ export class MethodHandlerBuilber {
                     statusText: "OK",
                     headers: {
                         "Allows": Object.keys(this.handlers).join(","),
+                        "Access-Control-Allow-Methods": req.headers.get(
+                            "Access-Control-Request-Method",
+                        )!,
+                        "Access-Control-Allow-Headers": req.headers.get(
+                            "Access-Control-Request-Headers",
+                        )!,
+                        "Access-Control-Allow-Origin": req.headers.get(
+                            "Origin",
+                        )!,
                     },
                 });
             }
