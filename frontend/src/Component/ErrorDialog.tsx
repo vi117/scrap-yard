1b9c7e1d2f4d9e37d13b265ab91d226c56fce168
Hyeonung Baek <wiserlix@gmail.com>
2022-06-02 23:04:12 +0900

    Add Logout Button and ErrorDialog

---
+++ frontend/src/Component/ErrorDialog.tsx
@@ -0,0 +1,34 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+} from "@mui/material";
+
+export function ErrorDialog(props: {
+    open: boolean;
+    onClose: () => void;
+    reason: string;
+}) {
+    const { open, onClose, reason } = props;
+
+    return (
+        <Dialog
+            open={open}
+            onClose={onClose}
+        >
+            <DialogContent>
+                <DialogContentText>
+                    {reason}
+                </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={onClose}>Close</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default ErrorDialog;
--- frontend/src/Page/UI.tsx
+++ frontend/src/Page/UI.tsx
@@ -1,3 +1,5 @@
+// main UI of the app.
+
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import { extname } from "path-browserify";
@@ -5,21 +7,44 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import DocumentEditor from "../Component/Document";
+import ErrorDialog from "../Component/ErrorDialog";
+import handleFile from "../Component/FileHandler";
 import FileTree from "../Component/FileTree";
 import Settings from "../Component/Settings";
 import ShareButton from "../Component/ShareButton";
 import Stash from "../Component/Stash";
 
+import { logout } from "../Model/login";
+
 const drawerWidth = 240;
+
+function LogoutButton() {
+    return (
+        <Button
+            variant="contained"
+            onClick={() => {
+                logout();
+                window.location.replace("/login");
+            }}
+        >
+            Logout
+        </Button>
+    );
+}
 
 export function UI(props: {}) {
     const params = useParams();
-    console.log(params);
 
     const [open, setOpen] = useState(false);
     const [sopen, setSopen] = useState(false);
-    const path = params.path + ".syd";
+    const [reason, setReason] = useState<null | string>(null);
     const [readonly, setReadonly] = useState(false);
+    const eopen = Boolean(reason);
+    const path = params.path + ".syd";
+
+    const raise = (e: Error) => {
+        setReason(e?.message);
+    };
 
     return (
         <div>
@@ -37,6 +62,7 @@ export function UI(props: {}) {
                     {readonly ? "readonly" : "writable"}
                 </Button>
                 <ShareButton doc={path} />
+                <LogoutButton />
             </Toolbar>
 
             <Settings open={sopen} onClose={() => setSopen(false)} />
@@ -44,14 +70,21 @@ export function UI(props: {}) {
                 width={drawerWidth}
                 handleFile={(com: string, f: string) => {
                     console.log(`${com} ${f}`);
+                    handleFile(com, { path: f }, raise);
                 }}
                 open={open}
                 onClose={() => setOpen(false)}
+                onError={raise}
                 root={""}
+            />
+            <ErrorDialog
+                open={eopen}
+                onClose={() => setReason(null)}
+                reason={reason}
             />
 
             <DocumentEditor readonly={readonly} path={path} />
-            <Stash />
+            {!readonly && <Stash />}
         </div>
     );
 }
